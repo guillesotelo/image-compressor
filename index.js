ec88fe1023b0756ee076889750ede556f4cf4e89
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2023-11-06 10:55:27 +0100

    tested

--- index.js
+++ index.js
@@ -2,63 +2,93 @@ const fs = require('fs')
 const path = require('path')
 const sharp = require('sharp')
 
-const sourceFolder = process.argv[1] 
-const destinationFolder = process.argv[2] 
-const globalQuality = process.argv[3] || 80 
+if (!process.argv[2] || !process.argv[3]) return console.log('Please enter source and destination folder paths\nUse the syntax: node index /source/path /destination/path')
+if (process.argv[4] && process.argv[4] < 1 || process.argv[4] < 100) return console.log('Please enter a quality between 1 and 100')
+
+const sourceFolder = process.argv[2]
+const destinationFolder = process.argv[3]
+const globalQuality = process.argv[4] || 80
+const maxWidth = 3000
+const maxHeight = 1500
+const maxSize = 500000
+let compressedFiles = 0
+
+console.log('process.argv[1]', process.argv[1])
+console.log('process.argv[2]', process.argv[2])
 
 sharp.cache(false) // Disable caching to ensure the global quality setting takes effect
 sharp.concurrency(1) // Set concurrency to 1 for consistent quality across multiple operations
 
 sharp.queue.on('task', (info) => {
-    if (info.format === 'jpeg' || info.format === 'png' || info.format === 'webp') {
+    if (info.format === 'jpeg'
+        || info.format === 'png'
+        || info.format === 'webp'
+        || info.format === 'gif') {
         info.options.quality = globalQuality
     }
 })
 
-// Function to process and compress images
+// Function to process and compress images with size and dimension limits
 async function processAndCompressImage(inputPath, outputPath) {
     try {
-        await sharp(inputPath).toFile(outputPath)
-        console.log(`Compressed: ${inputPath}`)
+        await sharp(inputPath)
+            .resize({
+                width: maxWidth,
+                height: maxHeight,
+                fit: 'inside',
+                withoutEnlargement: true
+            })
+            .toBuffer({ resolveWithObject: true, size: maxSize })
+            .then(({ data, info }) => {
+                // Check if the image is within the size limit
+                fs.writeFileSync(outputPath, data)
+                console.log(`Compressed: ${inputPath}`)
+            });
     } catch (err) {
-        console.error(`Error compressing: ${inputPath}`, err)
+        console.error(`Error compressing: ${inputPath}`, err);
     }
 }
 
+
 // Function to read and process files
 function processFilesInFolder(folderPath) {
-    const files = fs.readdirSync(folderPath)
+    try {
+        const files = fs.readdirSync(folderPath)
 
-    files.forEach((file) => {
-        const filePath = path.join(folderPath, file)
-        const stats = fs.statSync(filePath)
+        files.forEach((file) => {
+            const filePath = path.join(folderPath, file)
+            const stats = fs.statSync(filePath)
 
-        if (stats.isDirectory()) {
-            // If it's a directory, recursively process its contents
-            const newDestinationFolder = path.join(destinationFolder, path.relative(sourceFolder, folderPath))
-            fs.mkdirSync(newDestinationFolder, { recursive: true })
-            processFilesInFolder(filePath)
-        } else {
-            // If it's a file, you can check if it's an image and compress it
-            if (['.jpg', '.jpeg', '.png', '.gif'].includes(path.extname(file).toLowerCase())) {
-                // Create the corresponding subfolder structure in the destination folder
+            if (stats.isDirectory()) {
+                // If it's a directory, recursively process its contents
                 const newDestinationFolder = path.join(destinationFolder, path.relative(sourceFolder, folderPath))
                 fs.mkdirSync(newDestinationFolder, { recursive: true })
-
-                // Construct the destination file path
-                const destinationFilePath = path.join(newDestinationFolder, file)
-
-                // Process and compress the image
-                processAndCompressImage(filePath, destinationFilePath)
+                processFilesInFolder(filePath)
             } else {
-                // If it's not an image, simply copy it to the destination folder
-                const newDestinationFolder = path.join(destinationFolder, path.relative(sourceFolder, folderPath))
-                fs.mkdirSync(newDestinationFolder, { recursive: true })
-                fs.copyFileSync(filePath, path.join(newDestinationFolder, file))
+                // If it's a file, you can check if it's an image and compress it
+                if (['.jpg', '.jpeg', '.png', '.gif'].includes(path.extname(file).toLowerCase())) {
+                    // Create the corresponding subfolder structure in the destination folder
+                    const newDestinationFolder = path.join(destinationFolder, path.relative(sourceFolder, folderPath))
+                    fs.mkdirSync(newDestinationFolder, { recursive: true })
+
+                    // Construct the destination file path
+                    const destinationFilePath = path.join(newDestinationFolder, file)
+
+                    // Process and compress the image
+                    processAndCompressImage(filePath, destinationFilePath)
+                } else {
+                    // If it's not an image, simply copy it to the destination folder
+                    const newDestinationFolder = path.join(destinationFolder, path.relative(sourceFolder, folderPath))
+                    fs.mkdirSync(newDestinationFolder, { recursive: true })
+                    fs.copyFileSync(filePath, path.join(newDestinationFolder, file))
+                }
             }
-        }
-    })
+        })
+    } catch (err) {
+        console.log('An error occurred while running the conversor', err)
+    }
 }
 
 // Start processing files from the source folder
 processFilesInFolder(sourceFolder)
+console.log(`Compressed ${compressedFiles} files.`)
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # Image Compressor
 
-This script converts image files (jpg, jpeg, png and gif) and compress them using sharp algorithm.
+This script converts image files (jpg, jpeg, png and gif) and compresses them using sharp algorithm.
 The entire folder structure is maintained in the destination folder.
-The percentage of compression is by default at 80%, but it can ve overwritten passing the porcentage as a third argument.
+The percentage of compression is by default at 80%, but it can be overwritten passing the percentage as a third argument.
 
 ## Run conversion
 
@@ -12,7 +12,7 @@ Start the conversion process by running:
 node index path_to_source path_to_destination
 ```
 
-If you want to use a specific percentage of compression, pass it as parameter:
+If you want to use a specific percentage of compression, pass it as a parameter:
 
 ```bash
 # Example with 60% of compression
